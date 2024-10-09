0bda7768775fed4d8b2b5e45efb89f4a6b0ed3be
e.trier <e.trier@student.isartdigital.com>
2024-10-09 14:49:07 +0200

    Ally class created

---
+++ Assets/Scripts/Agents/Allies/Ally.cs
@@ -0,0 +1,19 @@
+using FSMMono;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : AIAgent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
